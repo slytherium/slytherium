f0c03b27130d6dfb801dcce17ed38b40f34e49bd
Rougin Royce Gutib <rougingutib@gmail.com>
2017-12-16 01:46:50 +0800

    Update IlluminateContainer

--- src/Provider/Illuminate/Container.php
+++ src/Provider/Illuminate/Container.php
@@ -5,6 +5,33 @@ namespace Slytherium\Provider\Illuminate;
 use Illuminate\Container\Container as Illuminate;
 use Slytherium\Container\ContainerInterface;
 
+/**
+ * Illuminate to Slytherium Bridge Container
+ *
+ * @package Slytherium
+ * @author  Rougin Royce Gutib <rougingutib@gmail.com>
+ */
 class Container extends Illuminate implements ContainerInterface
 {
+    /**
+     * Finds an entry of the container by its identifier and returns it.
+     *
+     * @param  string $id
+     * @return mixed
+     */
+    public function get($id)
+    {
+        return parent::get($id);
+    }
+
+    /**
+     * Returns true if the container can return an entry for the given identifier.
+     *
+     * @param  string $id
+     * @return boolean
+     */
+    public function has($id)
+    {
+        return parent::has($id);
+    }
 }
